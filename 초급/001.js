f70ae2b992d4acf11f21f6694f03c38cbe479037
문하연 <93499074+moonhayeon@users.noreply.github.com>
2021-12-01 21:29:28 +0900

    d1
    d1

---
+++ 초급/001.js
@@ -0,0 +1,14 @@
+var nums = [100, 200, 300, 400, 500];
+
+nums.splice(3);
+
+console.log(nums);
+
+/*
+var nums = [100, 200, 300, 400, 500];
+
+nums.pop();
+nums.pop();
+
+console.log(nums); 
+*/
---
+++ 초급/003.js
@@ -0,0 +1,5 @@
+var arr = [100, 200, 300];
+console.log(typeof(arr)); 
+// object
+
+// 자료형 object: 배열, 함수, 객체리터럴
