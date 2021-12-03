e83b728c7c385aeb27d7e13e07c0ca177729ae35
문하연 <93499074+moonhayeon@users.noreply.github.com>
2021-12-03 17:50:09 +0900

    d3
    d3

---
+++ 초급/008.js
@@ -0,0 +1,12 @@
+//객체리터럴
+
+var d = {
+    'height':180,
+    'weight':78,
+    'weight':84,
+    'temperature':36,
+    'eyesight':1
+};
+
+console.log(d['weight']); // 84
+//console.log(d.weight]);
